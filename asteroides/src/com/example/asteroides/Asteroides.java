e101a298983be6a82c8da34df9e1bc36ed33f037
Josefran <josefran@gmail.com>
2013-03-26 18:02:46 +0100

    Curso Android - Implementado Modulo 3

--- asteroides/src/com/example/asteroides/Asteroides.java
+++ asteroides/src/com/example/asteroides/Asteroides.java
@@ -1,23 +1,81 @@
 package com.example.asteroides;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Asteroides extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	public static AlmacenPuntuaciones almacen = new AlmacenPuntuacionesArray();
+	private Button bAcercaDe;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		bAcercaDe =(Button) findViewById(R.id.button3);
+		bAcercaDe.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				lanzarAcercaDe(null);
+			}
+		});
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.asteroides, menu);
-        return true;
-    }
-    
+	//	@Override
+	//	public boolean onCreateOptionsMenu(Menu menu) {
+	//		// Inflate the menu; this adds items to the action bar if it is present.
+	//		getMenuInflater().inflate(R.menu.asteroides, menu);
+	//		return true;
+	//	}
+
+	@Override 
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true; /** true -> el menú ya está visible */
+	}
+
+	@Override 
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.acercaDe:
+			lanzarAcercaDe(null);
+			break;
+		case R.id.config:
+			lanzarPreferencias(null);
+			break;
+		}
+		return true; /** true -> consumimos el item, no se propaga*/
+	}
+
+
+	public void lanzarAcercaDe(View view){
+		Intent i = new Intent(this, AcercaDe.class);
+		startActivity(i);
+	}
+
+	public void lanzarPreferencias(View view){
+		Intent i = new Intent(this, Preferencias.class);
+		startActivity(i);
+	}
+
+	public void lanzarPuntuaciones(View view) {
+		Intent i = new Intent(this, Puntuaciones.class);
+		startActivity(i);
+	}
+
+	public void lanzarExit(View view){
+		finish();
+	}
+
+
 }
+
+
